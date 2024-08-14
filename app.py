c52d1e4dd2f695c45bd3a87fde924c7338bacb15
cchanpanhavuth <chanpanhavuth.chea@gmail.com>
2024-08-14 16:09:56 +0700

    added confidence

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ app = Flask(__name__)
 vectorizer = joblib.load('vectorizer_opti.pkl')
 model = tf.keras.models.load_model('my_model_opti.h5')
 
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -26,8 +25,15 @@ def home():
 
         # Perform prediction
         prediction = model.predict(text_vectorized)
-        sentiment = 'Positive' if prediction[0][1] > 0.5 else 'Negative'
+        # Get the probability of the positive class
+        positive_prob = prediction[0][1]
+        sentiment = 'Positive' if positive_prob > 0.5 else 'Negative'
+        confidence = positive_prob if sentiment == 'Positive' else 1 - positive_prob
+
+        # Convert confidence to percentage
+        confidence_percentage = round(confidence * 100, 2)
         
-        return render_template('index.html', prediction=sentiment, text=text)
+        return render_template('index.html', prediction=sentiment, text=text, confidence_percentage=confidence_percentage)
     
-    return render_template('index.html', prediction=None, text=None)
+    return render_template('index.html', prediction=None, text=None, confidence_percentage=None)
+
